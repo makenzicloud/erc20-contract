03214eb9e76dab7f7f8a2bc22830a10df3287cda
makenzicloud <149491925+makenzicloud@users.noreply.github.com>
2024-03-05 17:25:05 +0000

    Create ERC20.sol

---
+++ ERC20.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.7.2/access/Ownable.sol";
+
+contract SafeRailsToken is ERC20, Ownable {
+
+    mapping(address => bool) public frozenAccounts;
+    
+    event AccountFrozen(address indexed _addr);
+    event AccountThawed(address indexed _addr); 
+
+    
+
+    
+
+    
+    
+
+
+
